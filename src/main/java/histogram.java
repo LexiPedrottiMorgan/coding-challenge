75aa2c1ea1d9b6a77542091d7758f88b936e1560
Lexi Morgan <leximorgan816@gmail.com>
2020-01-04 12:21:30 -0600

    init

---
+++ src/main/java/histogram.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class histogram {
+    public static void main(String[] args) {
+        generateHistogram();
+    }
+
+//    TODO: return type
+    public static void generateHistogram(){
+        System.out.println("hello world");
+    }
+}
